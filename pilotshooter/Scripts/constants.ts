2623df3bbee727ac33918b76668c415fb645839e
brewsterbhg <whenthingsexplode@gmail.com>
2014-11-10 00:11:30 -0500

    Added createjs and typescript support. Added base folder structure, game state constants, and a main game script. Also added styles.css

---
+++ pilotshooter/Scripts/constants.ts
@@ -0,0 +1,7 @@
+﻿module constants {
+    //State constants
+    export var MENU_STATE: number = 0;
+    export var PLAY_STATE: number = 1;
+    export var END_STATE: number = 2;
+    export var INSTRUCTION_STATE: number = 3;
+}  
--- pilotshooter/Scripts/game.ts
+++ pilotshooter/Scripts/game.ts
@@ -1,1 +1,45 @@
-﻿ 
+﻿/// <reference path="constants.ts" />
+
+//Variables
+var stage;
+var gameState;
+
+//Preload
+function preload() {
+
+}
+
+function init() {
+    //Set up canvas and ticker
+    stage = new createjs.Stage(document.getElementById("canvas"));
+    stage.enableMouseOver();
+    createjs.Ticker.setFPS(60);
+    createjs.Ticker.addEventListener("tick", gameLoop);
+
+    //Set initial game state
+    gameState = constants.MENU_STATE;
+    changeState(gameState);
+    //Start the game
+    startGame();
+}
+
+function changeState(state) {
+    switch (state) {
+        case constants.MENU_STATE:
+            break;
+        case constants.PLAY_STATE:
+            break;
+        case constants.END_STATE:
+            break;
+        case constants.INSTRUCTION_STATE:
+            break;
+    }
+}
+
+function startGame() {
+
+}
+
+function gameLoop() {
+    stage.update();
+}
